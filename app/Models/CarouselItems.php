df0cfb37385f82dc09956d34fa64766cbce809e3
chilleeee1234 <patosachille@gmail.com>
2023-11-14 13:09:18 +0800

    added migration and model

---
+++ app/Models/CarouselItems.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarouselItems extends Model
+{
+    use HasFactory;
+
+    //set preferences and option
+    //gets data from database
+    //pput table's name "carousel_items"
+    protected $table = 'carousel_items';
+
+    protected $primaryKey = 'carousel_item_id';
+
+    protected $fillable = [
+        'name',
+        'image_path',
+        'description',
+        'user_id',
+    ];
+}
