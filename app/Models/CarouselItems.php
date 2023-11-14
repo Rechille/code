985ec5b084a02ce7fbd86d769ae05713950f148e
chilleeee1234 <patosachille@gmail.com>
2023-11-14 13:47:57 +0800

    Controller, Route, Seeding

--- app/Models/CarouselItems.php
+++ app/Models/CarouselItems.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+//representation of our table : MODELS
+
 class CarouselItems extends Model
 {
     use HasFactory;
